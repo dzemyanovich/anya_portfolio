0286bba946e113578aed3e435260d964a293a563
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2022-11-13 20:04:26 +0300

    separete eventOptimizer component + css loader

---
+++ src/eventOptimizer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.css';
+
+function EventOptimizer() {
+    return (
+        <div className="title-wrapper">
+            Event Optimizer
+        </div>
+    );
+}
+
+export default EventOptimizer;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import EventOptimizer from './eventOptimizer';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div className="title-wrapper">
-        Event Optimizer
-    </div>
+    <EventOptimizer />
   </React.StrictMode>
 );
