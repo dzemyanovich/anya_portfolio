9335b04fb7d95e18079b59c1a901d51d63d68dae
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2022-11-14 01:12:09 +0300

    scrolling

--- src/eventOptimizer.js
+++ src/eventOptimizer.js
@@ -2,6 +2,15 @@ import React from 'react';
 import './app.css';
 
 function EventOptimizer() {
+    window.addEventListener('wheel', event => {
+        event.preventDefault();
+        event.stopPropagation();
+        const move = Math.abs(event.deltaY) > Math.abs(event.deltaX)
+            ? event.deltaY
+            : event.deltaX;
+        event.target.scrollLeft += move;
+    }, { passive:false });
+
     return (
         <div className="title-wrapper">
             Event Optimizer
