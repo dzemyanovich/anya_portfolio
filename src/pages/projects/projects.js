397698160d5248f179c26737635812f97d0a6373
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2022-11-20 01:36:34 +0300

    fully funcitonal projects page

--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -10,20 +10,79 @@ import zensupplies from '../../images/zensupplies.png';
 import designPlatform from '../../images/design-platform.png';
 import './projects.css';
 
-const Home = () => {
-  return <div className="projects">
+function scrollRight(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  const move = Math.abs(event.deltaY) > Math.abs(event.deltaX)
+      ? event.deltaY
+      : event.deltaX;
+  const linksContainer = document.getElementsByClassName('links-container')[0];
+  const proportion = linksContainer.clientWidth / linksContainer.clientHeight;
+  linksContainer.scrollLeft += move * proportion;
+  document.getElementsByClassName('projects')[0].scrollTop += move;
+}
+
+class Home extends React.Component {
+  componentDidMount() {
+      // hack: sometimes content of the page is not centered
+      window.scrollTo(0, 0);
+
+      window.addEventListener('wheel', scrollRight, { passive:false });
+  }
+
+  componentWillUnmount() {
+      window.removeEventListener('wheel', scrollRight, { passive:false });
+  }
+
+  render () {
+    return <div>
       <HomeLink />
-      <img src={burger} alt="burger" />
-      <Link className="title project-link" to='/event-optimizer-title'>Event Optimizer</Link>
-      <img src={supplyPlanning} alt="supply planning" />
-      <Link className="title project-link" to='/supply-planning-title'>Supply Planning</Link>
-      <img src={havi} alt="havi" />
-      <Link className="title project-link" to='/havi-title'>HAVI</Link>
-      <img src={designPlatform} alt="design platform" />
-      <Link className="title project-link" to='/design-platform-title'>Design Platform</Link>
-      <img src={zensupplies} alt="zensupplies" />
-      <Link className="title project-link" to='/zensupplies-title'>Zensupplies</Link>
+      <div className="links-container">
+        <div className="project-link-container">
+          <span>
+            <Link className="title" to='/event-optimizer-title'>Event Optimizer</Link>
+          </span>
+        </div>
+        <div className="project-link-container">
+          <span className="project-link">
+            <Link className="title" to='/supply-planning-title'>Supply Planning</Link>
+          </span>
+        </div>
+        <div className="project-link-container">
+          <span className="project-link">
+            <Link className="title" to='/havi-title'>HAVI</Link>
+          </span>
+        </div>
+        <div className="project-link-container">
+          <span className="project-link">
+            <Link className="title" to='/design-platform-title'>Design Platform</Link>
+          </span>
+        </div>
+        <div className="project-link-container">
+          <span className="project-link">
+            <Link className="title" to='/zensupplies-title'>Zensupplies</Link>
+          </span>
+        </div>
+      </div>
+      <div className="projects">
+        <div className="image-container">
+          <img src={burger} alt="burger" />
+        </div>
+        <div className="image-container">
+          <img src={supplyPlanning} alt="supply planning" />
+        </div>
+        <div className="image-container">
+          <img src={havi} alt="havi" />
+        </div>
+        <div className="image-container">
+          <img src={designPlatform} alt="design platform" />
+        </div>
+        <div className="image-container">
+          <img src={zensupplies} alt="zensupplies" />
+        </div>
+      </div>
     </div>;
-};
+  }
+}
 
 export default Home;
