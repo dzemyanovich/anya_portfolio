1a21955a7bb294117753e77fce8dfdbd7feff3bd
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2022-11-20 14:31:52 +0300

    home page animation

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -7,8 +7,12 @@ import './home.css';
 
 const Home = () => {
   return <div>
-    <img className="pivunova-brand" src={pivunovaBrand} width="150px" alt="pivunova brand" />
-    <div className="designer-title title">UX/UI Designer</div>
+    <div className="pivunova-brand">
+      <img className="pivunova-brand-content" src={pivunovaBrand} width="150px" alt="pivunova brand" />
+    </div>
+    <div className="designer-title-wrapper title">
+      <span className="designer-title">UX/UI Designer</span>
+    </div>
     <HomeLink />
     <Link className="page-link projects-link" to='/projects'>Projects</Link>
     <Link className="page-link about-link" to='/about'>About</Link>
