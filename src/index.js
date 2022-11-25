a928d1e109dfc010a2d2c477ad66fff9e1b607c7
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2022-11-25 19:42:29 +0300

    add one more todo

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
+    {/* TODO: consider switching back to BrowswerRouter */}
     <HashRouter>
       <Routes>
         <Route path='/' element={<Layout />}>
