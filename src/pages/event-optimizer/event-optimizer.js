c22cc006156f08cfa6283797e0fec3bbb8c16923
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2022-12-01 14:46:05 +0300

    add linting for js files

--- src/pages/event-optimizer/event-optimizer.js
+++ src/pages/event-optimizer/event-optimizer.js
@@ -10,7 +10,7 @@ export function EventOptimizerHeader() {
             <h3 className="project-title">About project</h3>
             <div className="project-description">
                 Event Optimizer is an information management platform for QSR industry dedicated to collect,
-                visually track and analyze key data points of McDonald's & Subway based on data science
+                visually track and analyze key data points of McDonald&apos;s & Subway based on data science
                 (marketing / stock / sales / results of previous promotions / risks, etc.) for automated intelligent
                 forecasting. The tool is fully customizable to meet the specific needs of a department and company
                 and ...
