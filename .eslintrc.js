027aaf0c98468287030591de5950091422014a4a
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2022-12-01 20:22:56 +0300

    fix indentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,32 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "airbnb",
-        "airbnb/hooks"
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "object-curly-newline": "off",
-        "react/jsx-wrap-multilines": "off",
-        "max-len": ["error", { "code": 120 }],
-        "arrow-parens": "off"
-    },
-    "settings": {
-        "react": {
-            "version": "detect"
-        }
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "airbnb",
+    "airbnb/hooks"
+  ],
+  "overrides": [
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "object-curly-newline": "off",
+    "react/jsx-wrap-multilines": "off",
+    "max-len": ["error", { "code": 120 }],
+    "arrow-parens": "off"
+  },
+  "settings": {
+    "react": {
+      "version": "detect"
     }
+  }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,48 @@
-const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
-const path = require( 'path' );
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-   context: __dirname,
-   entry: './src/index.jsx',
-   output: {
-      path: path.resolve( __dirname, 'dist' ),
-      filename: 'main.js',
-      publicPath: '/',
-   },
-   devServer: {
-      historyApiFallback: true
-   },
-   module: {
-      rules: [
-         {
-            test: /\.jsx?$/,
-            use: 'babel-loader',
-         },
-         {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader'],
-         },
-         {
-            test: /\.scss$/,
-            use: ['style-loader', 'css-loader', 'sass-loader'],
-          },
-         {
-            test: /\.(png|j?g|svg|gif)?$/,
-            use: 'file-loader'
-         }
-]
-   },
-   resolve: {
-      extensions: ['', '.js', '.jsx'],
-   },
-   plugins: [
-      new HtmlWebPackPlugin({
-         template: path.resolve( __dirname, 'src/index.html' ),
-         filename: 'index.html'
-      })
-   ],
-   performance: {
-      // TODO: fix all performance warnings
-      hints: false
+  context: __dirname,
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: '/',
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|j?g|svg|gif)?$/,
+        use: 'file-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: 'index.html'
+    })
+  ],
+  performance: {
+    // TODO: fix all performance warnings
+    hints: false
   }
 };
