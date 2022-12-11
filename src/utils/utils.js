5f7d4dfd12b72dc23e75fd020bc1fceb766f685a
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2022-12-11 19:22:37 +0300

    do not show scrollbar everywhere

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,9 +1,5 @@
 const isWindowsValue = navigator.platform.indexOf('Win') > -1;
 
-if (isWindowsValue) {
-  document.querySelector('html').classList.add('no-scrollbar');
-}
-
 export function isWindows() {
   return isWindowsValue;
 }
