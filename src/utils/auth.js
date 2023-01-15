4b4c7cf51b2aa9a999a792a3de8d59b2612c14ec
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2023-01-15 20:49:21 +0300

    use aws api (manual one)

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,16 +2,32 @@ const TOKEN_VAR = 'token';
 const isAuthenticatedValue = localStorage.getItem(TOKEN_VAR);
 
 export function login(password) {
-  // todo: make call to AWS lambda
   return new Promise((resolve) => {
-    setTimeout(() => {
-      if (password === 'asdf') {
-        localStorage.setItem(TOKEN_VAR, 'some_token_will_be_here');
-        resolve(true);
-      } else {
-        resolve(false);
-      }
-    }, 1000);
+    // todo: enable cors for infra created via terraform
+    // const url = 'https://lxe4b8taqj.execute-api.eu-central-1.amazonaws.com/prod/auth';
+    // todo: store url in config
+    const url = 'https://7b1gputb22.execute-api.eu-central-1.amazonaws.com/prod/my_api_resource';
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        password,
+      }),
+    }).then((response) => {
+      response.text().then((text) => {
+        const data = text && JSON.parse(text);
+
+        if (data.isCorrectPassword) {
+          localStorage.setItem(TOKEN_VAR, 'some_token_will_be_here');
+          resolve(true);
+        } else {
+          resolve(false);
+        }
+      });
+    });
   });
 }
 
