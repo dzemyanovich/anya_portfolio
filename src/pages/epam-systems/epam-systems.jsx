a983d5d318632072f9c86ea76b847968c8e2bec5
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2023-01-24 15:48:55 +0300

    add content for epam systems page

--- src/pages/epam-systems/epam-systems.jsx
+++ src/pages/epam-systems/epam-systems.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 import image from '../../images/old-computer.png';
-import background from '../../images/event-optimizer-background.png';
+// todo: use another image
+import epamLogo from '../../images/epam-logo.png';
+import aboutImage from '../../images/design-portfolio-hints.jpg';
+// todo: use original image but not screenshot
+import resultsImage from '../../images/design-portfolio-code.png';
+// todo: image's quality is too low
+import bottomImage from '../../images/design-portfolio-components.jpg';
+import './epam-systems.scss';
 
 export function EpamSystemsHeader() {
   return [
@@ -20,7 +27,65 @@ export function EpamSystemsHeader() {
 }
 
 export function EpamSystemsContent() {
-  return [
-    <img src={background} alt="" key="page-content-background" className="event-optimizer-background" />,
-  ];
+  return (
+    <div>
+      <img src={epamLogo} alt="" className="epam-logo" />
+      <div className="about-container">
+        <div>
+          <div>
+            <div>Design Platform</div>
+            <div className="paragraph-title">
+              About project
+            </div>
+            <div>
+              Digital Platform is EPAM&apos;s internal ecosystem, making work and life of employees easier and efficient.
+              Currently, the EPAM product ecosystem has more than 150 projects. Supporting such a huge number of
+              products seems like an overwhelming task, requiring a huge amount of resources, because each of them
+              is unique and must still be perceived as part of one ecosystem. Design Platform is aimed at achieving
+              this goal. This initiative is not represented only by a system of components, but by a series of
+              measures to change the outlook and the revolution in the organization of work.
+            </div>
+          </div>
+          <div>
+            <div className="paragraph-title">Challenges</div>
+            <div>
+              <ul>
+                <li>Lack of consistency between products & various design libraries inside EPAM</li>
+                <li>More than 20 stakeholders</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <img src={aboutImage} alt="" className="about-image" />
+      </div>
+      <div className="results-container">
+        <div className="results">
+          <div className="paragraph-title">Results</div>
+          <div>
+            <ul>
+              <li>
+                Based on competitive benchmarking of popular design systems, upgraded simple UI kit and React
+                framework into a scalable design system with guidelines & component editor, consistent & complied
+                with EPAM branding. That allows designers and developers to create a user interface 4X faster &
+                reduce the costs of the design & development on the streams.
+              </li>
+              <li>
+                Process enhancement based on DesignOps practice helped significantly reduce effort and optimize the
+                design team with more than 13 active products from 13 to 3 designers
+              </li>
+              <li>
+                Safe & easy on-boarding/rotation process: newcomers dive into the project&apos;s
+                UX/UI ecosystem 7X faster
+              </li>
+              <li>
+                Double-development dropped to zero due to a single source of truth
+              </li>
+            </ul>
+          </div>
+        </div>
+        <img src={resultsImage} alt="" className="results-image" />
+      </div>
+      <img src={bottomImage} alt="" className="bottom-image" />
+    </div>
+  );
 }
