079fd965a4e8610e7bc3067b28fa68011ecdd4b9
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2023-02-09 15:00:15 +0300

    home-link mobile animation on about page

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -9,27 +9,29 @@ export default function About() {
   return (
     <div className="about-container">
       <HomeLink />
-      <div className="about-image-container">
-        <div className="about-name title">Anna Pivunova</div>
-        <div className="about-title title">Lead Designer</div>
-        <img src={aboutImage} alt="" className="about-image" />
-      </div>
-      <div className="about-content">
-        <div className="paragraph">
-          <div className="paragraph-title">About me</div>
-          <div className="paragraph-content">
-            Adidas is a German multinational corporation, founded and headquartered in Herzogenaurach, Bavaria,
-            that designs and manufactures shoes, clothing and accessories. It is the largest sportswear
-            manufacturer in EuropeAdidas is a German multinational corporation, founded and headquartered in
-            Herzogenaurach, Bavaria, that designs as
-          </div>
+      <div className="about-page">
+        <div className="about-image-container">
+          <div className="about-name title">Anna Pivunova</div>
+          <div className="about-title title">Lead Designer</div>
+          <img src={aboutImage} alt="" className="about-image" />
         </div>
-        <div className="paragraph">
-          <div className="paragraph-title">Clients</div>
-          <div className="paragraph-content">
-            Adidas is a German multinational corporation, founded and headquartered in Herzogenaurach, Bavaria,
-            that designs and manufactures shoes, clothing and accessories. It is the largest sportswear
-            manufacturer in EuropeAdidas is a German multinational corporation, founded and
+        <div className="about-content">
+          <div className="paragraph">
+            <div className="paragraph-title">About me</div>
+            <div className="paragraph-content">
+              Adidas is a German multinational corporation, founded and headquartered in Herzogenaurach, Bavaria,
+              that designs and manufactures shoes, clothing and accessories. It is the largest sportswear
+              manufacturer in EuropeAdidas is a German multinational corporation, founded and headquartered in
+              Herzogenaurach, Bavaria, that designs as
+            </div>
+          </div>
+          <div className="paragraph">
+            <div className="paragraph-title">Clients</div>
+            <div className="paragraph-content">
+              Adidas is a German multinational corporation, founded and headquartered in Herzogenaurach, Bavaria,
+              that designs and manufactures shoes, clothing and accessories. It is the largest sportswear
+              manufacturer in EuropeAdidas is a German multinational corporation, founded and
+            </div>
           </div>
         </div>
       </div>
