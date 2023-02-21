721e54b91a5be1a1ee6730cda324cd48fdf8164b
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2023-02-21 14:39:14 +0300

    add tests for utils -> isWindows

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -5,17 +5,43 @@ describe('isTouchDevice', () => {
     delete window.ontouchstart;
   });
 
-  test('returns false', () => {
-    const { isTouchDevice } = require('./utils');
-
-    expect(isTouchDevice()).toBe(false);
-  });
-
   test('returns true', () => {
     window.ontouchstart = () => {};
 
     const { isTouchDevice } = require('./utils');
 
     expect(isTouchDevice()).toBe(true);
+  });
+
+  test('returns false', () => {
+    const { isTouchDevice } = require('./utils');
+
+    expect(isTouchDevice()).toBe(false);
+  });
+});
+
+describe('isWindows', () => {
+  let platformGetter;
+
+  beforeEach(() => {
+    platformGetter = jest.spyOn(window.navigator, 'platform', 'get');
+
+    jest.resetModules();
+  });
+
+  test('returns true', () => {
+    platformGetter.mockReturnValue('Win');
+
+    const { isWindows } = require('./utils');
+
+    expect(isWindows()).toBe(true);
+  });
+
+  test('returns false', () => {
+    platformGetter.mockReturnValue('Mac');
+
+    const { isWindows } = require('./utils');
+
+    expect(isWindows()).toBe(false);
   });
 });
