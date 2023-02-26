6b23baf0e2dcc0f179f86236ef4f7178bf1037dd
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2023-02-26 15:49:51 +0300

    tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,17 @@
 terraform {
-  # todo: delete commented code
-  # required_providers {
-  #   aws = {
-  #     source  = "hashicorp/aws"
-  #     version = "~> 4.16"
-  #   }
-  # }
-
-  backend "s3" {
-    bucket = "anya-portfolio-tf-state"
-    key    = "anya-portfolio.tfstate"
-    region = "eu-central-1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
   }
+
+  # todo: delete commented code
+  # backend "s3" {
+  #   bucket = "anya-portfolio-tf-state"
+  #   key    = "anya-portfolio.tfstate"
+  #   region = "eu-central-1"
+  # }
 
   required_version = ">= 1.2.0"
 }
