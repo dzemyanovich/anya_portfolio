8d4263a362a6691ab96e13d8627a3d257d6a4c84
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2023-03-02 23:12:56 +0300

    add more s3 config for terragrunt

--- terragrunt/live/terragrunt.hcl
+++ terragrunt/live/terragrunt.hcl
@@ -6,6 +6,8 @@ locals {
 remote_state {
   backend = "s3"
   config = {
+    # todo: make renaming
+    # bucket = "${local.env}.annapivunova.me-terragrunt"
     bucket = "${local.env}.annapivunova.me-remote-state"
     region = "${local.region}"
     key    = "${path_relative_to_include()}/terraform.tfstate"
--- terragrunt/modules/s3/main.tf
+++ terragrunt/modules/s3/main.tf
@@ -1,3 +1,7 @@
+locals {
+  mime_types = jsondecode(file("${path.module}/data/mime.json"))
+}
+
 resource "aws_s3_bucket" "website_bucket" {
   bucket = "${var.website_bucket_name}"
  
@@ -16,4 +20,31 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
   error_document {
     key = "index.html"
   }
+}
+
+resource "aws_s3_bucket_policy" "website_bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+  policy = templatefile("data/s3-policy.json", { bucket = "${var.website_bucket_name}" })
+}
+
+resource "aws_s3_bucket_acl" "website_bucket_acl" {
+  bucket = aws_s3_bucket.website_bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_ownership_controls" "website_bucket_ownnership" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_object" "source_code" {
+  for_each     = fileset("../../../../dist_preprod/", "*")
+  bucket       = aws_s3_bucket.website_bucket.id
+  key          = each.value
+  source       = "../../../../dist_preprod/${each.value}"
+  etag         = filemd5("../../../../dist_preprod/${each.value}")
+  content_type = lookup(local.mime_types, regex("\\.(?P<extension>[A-Za-z0-9]+)$", each.value).extension, "application/octet-stream")
 }
