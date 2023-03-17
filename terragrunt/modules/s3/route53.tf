6851bc4e4b685c01a52c76d6bd8e7394fcae8fbf
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2023-03-17 17:20:25 +0300

    test dns

--- terragrunt/modules/s3/route53.tf
+++ terragrunt/modules/s3/route53.tf
@@ -1,19 +1,19 @@
-resource "aws_route53_zone" "website_zone" {
-  # create route53 only for prod env
-  count = "${var.is_prod_env ? 1 : 0}"
-  name  = var.website_bucket_name
-}
+# resource "aws_route53_zone" "website_zone" {
+#   # create route53 only for prod env
+#   count = "${var.is_prod_env ? 1 : 0}"
+#   name  = var.website_bucket_name
+# }
 
-resource "aws_route53_record" "s3_record" {
-  # create route53 only for prod env
-  count   = "${var.is_prod_env ? 1 : 0}"
-  zone_id = aws_route53_zone.website_zone[0].zone_id
-  name    = var.website_bucket_name
-  type    = "A"
+# resource "aws_route53_record" "s3_record" {
+#   # create route53 only for prod env
+#   count   = "${var.is_prod_env ? 1 : 0}"
+#   zone_id = aws_route53_zone.website_zone[0].zone_id
+#   name    = var.website_bucket_name
+#   type    = "A"
 
-  alias {
-    name                   = aws_s3_bucket_website_configuration.website_configuration.website_endpoint
-    zone_id                = aws_s3_bucket.website_bucket.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
+#   alias {
+#     name                   = aws_s3_bucket_website_configuration.website_configuration.website_endpoint
+#     zone_id                = aws_s3_bucket.website_bucket.hosted_zone_id
+#     evaluate_target_health = false
+#   }
+# }
