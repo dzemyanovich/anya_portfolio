d5ccae25672b43e067822454dd697481c7f5bc46
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2023-03-18 20:29:45 +0300

    add www route53

--- terragrunt/modules/s3/route53.tf
+++ terragrunt/modules/s3/route53.tf
@@ -17,3 +17,17 @@ resource "aws_route53_record" "s3_record" {
     evaluate_target_health = false
   }
 }
+
+resource "aws_route53_record" "www_s3_record" {
+  # create route53 only for prod env
+  count   = "${var.is_prod_env ? 1 : 0}"
+  zone_id = aws_route53_zone.website_zone[0].zone_id
+  name    = "www.${var.website_bucket_name}"
+  type    = "A"
+
+  alias {
+    name                   = aws_s3_bucket_website_configuration.website_configuration.website_endpoint
+    zone_id                = aws_s3_bucket.website_bucket.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
