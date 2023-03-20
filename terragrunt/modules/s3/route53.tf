22db58e54003c9c300a5d0f9400408ecab9ac7e5
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2023-03-20 23:31:38 +0300

    asdf

--- terragrunt/modules/s3/route53.tf
+++ terragrunt/modules/s3/route53.tf
@@ -31,29 +31,29 @@ resource "aws_route53_record" "www_s3_record" {
   }
 }
 
-resource "aws_acm_certificate" "ssl_certificate" {
-  count                     = "${var.is_prod_env ? 1 : 0}"
-  domain_name               = var.website_bucket_name
-  subject_alternative_names = [ "www.${var.website_bucket_name}", "*.${var.website_bucket_name}" ]
-  validation_method         = "DNS"
+# resource "aws_acm_certificate" "ssl_certificate" {
+#   count                     = "${var.is_prod_env ? 1 : 0}"
+#   domain_name               = var.website_bucket_name
+#   subject_alternative_names = [ "www.${var.website_bucket_name}", "*.${var.website_bucket_name}" ]
+#   validation_method         = "DNS"
 
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
 
-resource "aws_route53_record" "ssl_certificate_dns" {
-  count           = "${var.is_prod_env ? 1 : 0}"
-  allow_overwrite = true
-  name            =  tolist(aws_acm_certificate.ssl_certificate[0].domain_validation_options)[0].resource_record_name
-  records         = [tolist(aws_acm_certificate.ssl_certificate[0].domain_validation_options)[0].resource_record_value]
-  type            = tolist(aws_acm_certificate.ssl_certificate[0].domain_validation_options)[0].resource_record_type
-  zone_id         = aws_route53_zone.website_zone[0].zone_id
-  ttl             = 60
-}
+# resource "aws_route53_record" "ssl_certificate_dns" {
+#   count           = "${var.is_prod_env ? 1 : 0}"
+#   allow_overwrite = true
+#   name            =  tolist(aws_acm_certificate.ssl_certificate[0].domain_validation_options)[0].resource_record_name
+#   records         = [tolist(aws_acm_certificate.ssl_certificate[0].domain_validation_options)[0].resource_record_value]
+#   type            = tolist(aws_acm_certificate.ssl_certificate[0].domain_validation_options)[0].resource_record_type
+#   zone_id         = aws_route53_zone.website_zone[0].zone_id
+#   ttl             = 60
+# }
 
-resource "aws_acm_certificate_validation" "ssl_certificate_validate" {
-  count                   = "${var.is_prod_env ? 1 : 0}"
-  certificate_arn         = aws_acm_certificate.ssl_certificate[0].arn
-  validation_record_fqdns = aws_route53_record.ssl_certificate_dns.*.fqdn
-}
+# resource "aws_acm_certificate_validation" "ssl_certificate_validate" {
+#   count                   = "${var.is_prod_env ? 1 : 0}"
+#   certificate_arn         = aws_acm_certificate.ssl_certificate[0].arn
+#   validation_record_fqdns = aws_route53_record.ssl_certificate_dns.*.fqdn
+# }
