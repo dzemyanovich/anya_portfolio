55626b8c702c78a489a7c1599fc434d68bf60cf0
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2023-03-21 16:18:56 +0300

    cloudformation attempt #1

--- terragrunt/modules/s3/route53.tf
+++ terragrunt/modules/s3/route53.tf
@@ -1,7 +1,8 @@
 # everything below is created only for prod env
 
 locals {
-  domain_alt_names = [ "www.${var.website_bucket_name}", "*.${var.website_bucket_name}" ]
+  domain_alt_names = ["www.${var.website_bucket_name}", "*.${var.website_bucket_name}"]
+  s3_origin_id     = "my_s3_origin"
 }
 
 resource "aws_route53_zone" "website_zone" {
@@ -67,4 +68,74 @@ resource "aws_acm_certificate_validation" "ssl_certificate_validate" {
   provider                = aws.virginia
   certificate_arn         = aws_acm_certificate.ssl_certificate[0].arn
   validation_record_fqdns = aws_route53_record.ssl_certificate_dns.*.fqdn
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  count = "${var.is_prod_env ? 1 : 0}"
+
+  origin {
+    domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Some comment"
+  default_root_object = "index.html"
+
+  logging_config {
+    include_cookies = false
+    bucket          = "${var.website_bucket_name}.s3.amazonaws.com"
+    prefix          = "cloudfront_logs"
+  }
+
+  aliases = ["${var.website_bucket_name}","www.${var.website_bucket_name}"]
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = "${aws_acm_certificate.ssl_certificate[0].arn}"
+    ssl_support_method = "sni-only"
+  }
+
+  depends_on = [aws_acm_certificate.ssl_certificate[0]]
+}
+
+resource "aws_route53_record" "cloud_front_record" {
+  count   = "${var.is_prod_env ? 1 : 0}"
+  zone_id = aws_route53_zone.website_zone[0].zone_id
+  name    = var.website_bucket_name
+  type    = "A"
+
+  alias {
+    name                   = replace(aws_cloudfront_distribution.s3_distribution[0].domain_name, "/[.]$/", "")
+    zone_id                = "${aws_cloudfront_distribution.s3_distribution[0].hosted_zone_id}"
+    evaluate_target_health = true
+  }
+
+  depends_on = [aws_cloudfront_distribution.s3_distribution[0]]
 }
