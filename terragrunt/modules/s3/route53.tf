350761f83d2b345162f7b3ce861e6b577a9bcb8a
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2023-03-22 07:38:08 +0300

    asdf

--- terragrunt/modules/s3/route53.tf
+++ terragrunt/modules/s3/route53.tf
@@ -54,6 +54,13 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
     domain_name = aws_s3_bucket_website_configuration.website_configuration.website_endpoint
     origin_id   = local.s3_origin_id
+
+    custom_origin_config {
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
   }
 
   enabled             = true
